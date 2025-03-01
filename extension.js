92b3b2c908b6efa86533ca12737a8ab0476382ea
eypsilon <engin@ok.de>
2025-03-01 22:39:50 +0100

    Final

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Util from 'resource:///org/gnome/shell/misc/util.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
@@ -41,13 +41,8 @@ const ColorPickerIndicator = GObject.registerClass(
 
         _runColorPicker() {
             let scriptPath = this._getExtensionPath() + "/scripts/colorpicker.sh";
-            GLib.spawn_async(
-                null,
-                ["/usr/bin/gnome-terminal", "--", "bash", "-c", scriptPath],
-                null,
-                GLib.SpawnFlags.SEARCH_PATH,
-                null
-            );
+
+            Util.spawn(["gnome-terminal", "--", "bash", "-c", scriptPath]);
         }
     }
 );
